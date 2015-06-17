c3a5eb78563418dfe970c019fd983c592c40a4cb
Harold Angenent <harold@trendwerk.nl>
2015-06-17 10:54:15 +0200

    Better approach to submenu filter

--- class-tp-submenu.php
+++ class-tp-submenu.php
@@ -12,9 +12,14 @@
  */
 
 class TP_Submenu_Plugin {
+	/**
+	 * Last parsed title
+	 */
+	public $title;
 
 	function __construct() {
 		add_action( 'plugins_loaded', array( $this, 'localization' ) );
+		add_filter( 'wp_nav_menu_objects', array( $this, 'submenu' ), 10, 2 );
 	}
 
 	/**
@@ -24,17 +29,62 @@ class TP_Submenu_Plugin {
 		load_muplugin_textdomain( 'widget-submenu', dirname( plugin_basename( __FILE__ ) ) . '/assets/lang/' );
 	}
 
-} new TP_Submenu_Plugin;
+	/**
+	 * Submenu support for wp_nav_menu
+	 */
+	function submenu( $items, $args ) {
+		if( ! isset( $args->submenu ) || ! $args->submenu )
+			return $items;
+
+		$current = array_pop( ( wp_filter_object_list( $items, array( 'current' => true ) ) ) );
+
+		if( ! isset( $current ) )
+			return array();
+
+		$parent = $this->get_top( $current, $items );
+		$this->title = $parent->title;
+
+		return $this->get_children( $parent, $items );
+	}
+
+	/**
+	 * Get top parent
+	 */
+	function get_top( $item, $items ) {
+		if( ! isset( $item->menu_item_parent ) )
+			return $item;
+
+		while( 0 < $item->menu_item_parent )
+			$item = array_pop( ( wp_filter_object_list( $items, array( 'ID' => $item->menu_item_parent ) ) ) );
+
+		return $item;
+	}
+
+	/**
+	 * Get all children
+	 */
+	function get_children( $parent, $items ) {
+		$children = wp_filter_object_list( $items, array( 'menu_item_parent' => $parent->ID ) );
+
+		if( 0 === count( $children ) )
+			return array();
+
+		foreach( $children as $child )
+			$children = array_merge( $children, $this->get_children( $child, $items ) );
+
+		return $children;
+	}
+
+}
+
+$GLOBALS['tp_submenu_plugin'] = new TP_Submenu_Plugin();
 
 class TP_Submenu extends WP_Widget {
-	var $title;
-	
+
 	function __construct() {
 		$this->WP_Widget( 'TP_Submenu', __( 'Submenu', 'widget-submenu' ), array(
 			'description' => __( 'Shows submenu items of current menu item or parent.', 'widget-submenu' )
 		) );
-
-		add_filter( 'wp_nav_menu_objects', array( $this, 'submenu' ), 10, 2 );
 	}
 
 	function form( $instance ) {
@@ -90,60 +140,17 @@ class TP_Submenu extends WP_Widget {
 		if( 0 === strlen( $menu ) )
 			return;
 
+		global $tp_submenu_plugin;
+
 		echo $before_widget;
 
-			echo $before_title . $this->title . $after_title;
+			echo $before_title . $tp_submenu_plugin->title . $after_title;
 
 			echo $menu;
 
 		echo $after_widget;
 	}
 
-	/**
-	 * Submenu support for wp_nav_menu
-	 */
-	function submenu( $items, $args ) {
-		if( ! isset( $args->submenu ) || ! $args->submenu )
-			return $items;
-
-		$current = array_pop( ( wp_filter_object_list( $items, array( 'current' => true ) ) ) );
-
-		if( ! isset( $current ) )
-			return array();
-
-		$parent = $this->get_top( $current, $items );
-		$this->title = $parent->title;
-
-		return $this->get_children( $parent, $items );
-	}
-
-	/**
-	 * Get top parent
-	 */
-	function get_top( $item, $items ) {
-		if( ! isset( $item->menu_item_parent ) )
-			return $item;
-
-		while( 0 < $item->menu_item_parent )
-			$item = array_pop( ( wp_filter_object_list( $items, array( 'ID' => $item->menu_item_parent ) ) ) );
-
-		return $item;
-	}
-
-	/**
-	 * Get all children
-	 */
-	function get_children( $parent, $items ) {
-		$children = wp_filter_object_list( $items, array( 'menu_item_parent' => $parent->ID ) );
-
-		if( 0 === count( $children ) )
-			return array();
-
-		foreach( $children as $child )
-			$children = array_merge( $children, $this->get_children( $child, $items ) );
-
-		return $children;
-	}
 }
 
 add_action( 'widgets_init', function() {
